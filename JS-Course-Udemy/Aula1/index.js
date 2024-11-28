280b297299bfeee2d9cc52a02eabcc104877d19e
Henrique <103958824+HenriqueSales1@users.noreply.github.com>
2024-11-28 16:50:34 -0300

    Curso Udemy
    Início do curso de JS/TS na Udemy

---
+++ JS-Course-Udemy/Aula1/index.js
@@ -0,0 +1,12 @@
+console.log('Hello world!');
+
+// No js, todo tipo de input dentro de aspas é uma string
+
+console.log('Teste');
+console.log("'Teste'");
+console.log(`'Mais' de uma "string"`);
+
+// Separa-se argumentos com vírgulas
+
+console.log(123456, 1231512315.231231); // Não existem declarações como int ou float em js
+console.log(15 /* number */, 13.84 /* number */, 'Henrique' /* string */);
