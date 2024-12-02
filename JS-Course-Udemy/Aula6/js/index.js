ed7e9b3b8e513173dc70ab0b18e45473b48c4713
Henrique <103958824+HenriqueSales1@users.noreply.github.com>
2024-12-01 22:06:41 -0300

    Curso js

---
+++ JS-Course-Udemy/Aula6/js/index.js
@@ -0,0 +1,21 @@
+// Não é possível criar nomes de variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não começar nomes de variáveis com números
+// Não podem conter espaços/traços (hifens)
+// Utilizar camelCase
+// Js é case-sensitive -> "let nomeCliente" e "let nomecliente" são duas variáveis diferentes
+// Não podemos redeclarar variáveis com let
+// UTILIZAR LET SOBRE VAR
+
+let nome;
+nome = 'Henrique';
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve um filho com', nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo.');
+
+// nome = 'Henricos';
+
+// console.log(nome);
