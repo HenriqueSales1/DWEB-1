af88c63081a83d539d68c94b0582f089e52389c9
Henrique <103958824+HenriqueSales1@users.noreply.github.com>
2024-12-03 10:24:09 -0300

    Intro JS

--- Aula-12-03/dist/functions.dev.js
+++ Aula-12-03/dist/functions.dev.js
@@ -11,6 +11,7 @@ function testesTipos() {
   var f = {};
   var g = "Lucas";
   var h = "Igor";
+  var i = "".concat(d, ", ").concat(g, " e ").concat(h, " s\xE3o estudantes.");
   console.log(_typeof(a));
   console.log(_typeof(b));
   console.log(_typeof(c));
@@ -19,4 +20,99 @@ function testesTipos() {
   console.log(_typeof(f));
   console.log(_typeof(g));
   console.log(_typeof(h));
+  console.log(i);
+  console.log(Number.parseInt("1") + 1);
+}
+
+function testesLetConstVar() {
+  if (true) {
+    var a = 10;
+  }
+
+  console.log(a);
+
+  if (true) {
+    var b = 11;
+    console.log(b);
+  }
+
+  if (true) {
+    var c = 12;
+    console.log(c);
+  }
+
+  var v = [3, 6, 16, 7];
+  v[0] = 10;
+  console.log(v);
+}
+
+function testesControle() {
+  var opt = confirm("Certeza?");
+
+  if (opt) {
+    alert("Feito.");
+  } else {
+    alert("Cancelado.");
+  }
+}
+
+function testesRepeticao() {
+  for (var _i = 0; _i < 25; _i++) {
+    console.log(_i * 2);
+  }
+
+  var i = 100;
+
+  while (i < 120) {
+    console.log(i);
+    i++;
+  }
+}
+
+function testeObjeto() {
+  var pessoa = {
+    nome: "Henrique",
+    idade: 21,
+    cpf: "123.456.789-00"
+  };
+  console.log(_typeof(pessoa));
+  console.log(pessoa);
+  console.log(pessoa.nome);
+  console.log(pessoa.idade);
+  console.log(pessoa.cpf);
+}
+
+function testeObjeto2() {
+  var generico = [12, "Henrique", 45];
+  console.log(_typeof(generico));
+  console.log(generico);
+
+  for (var i = 0; i < generico.length; i++) {
+    console.log(generico[i]);
+  }
+}
+
+function testeArrayComObjeto() {
+  var pessoas = [{
+    "nome": "Rosinei",
+    "idade": 21
+  }, {
+    "nome": "Igor",
+    "idade": 21
+  }, {
+    "nome": "Lucas",
+    "idade": 21
+  }];
+
+  for (var i = 0; i < pessoas.length; i++) {
+    console.log(pessoas[i].nome, ': ', pessoas[i].idade);
+  }
+}
+
+function testeArrayComObjeto2() {
+  var pessoas = {
+    "nome": "Ilda",
+    "idade": 45,
+    "dependentes": ['Ana', 'Paulo', 'Isaias']
+  };
 }
--- Aula-12-03/functions.js
+++ Aula-12-03/functions.js
@@ -7,6 +7,7 @@ function testesTipos() {
   let f = {};
   let g = "Lucas";
   let h = `Igor`;
+  let i = `${d}, ${g} e ${h} são estudantes.`;
   console.log(typeof a);
   console.log(typeof b);
   console.log(typeof c);
@@ -15,4 +16,93 @@ function testesTipos() {
   console.log(typeof f);
   console.log(typeof g);
   console.log(typeof h);
+  console.log(i);
+  console.log(Number.parseInt("1") + 1);
+}
+
+function testesLetConstVar() {
+  if (true) {
+    var a = 10;
+  }
+  console.log(a);
+
+  if (true) {
+    let b = 11;
+    console.log(b);
+  }
+
+  if (true) {
+    const c = 12;
+    console.log(c);
+  }
+
+  const v = [3, 6, 16, 7];
+  v[0] = 10;
+  console.log(v);
+}
+
+function testesControle() {
+  const opt = confirm("Certeza?");
+  if (opt) {
+    alert("Feito.");
+  } else {
+    alert("Cancelado.");
+  }
+}
+
+function testesRepeticao() {
+  for (let i = 0; i < 25; i++) {
+    console.log(i * 2);
+  }
+  let i = 100;
+  while (i < 120) {
+    console.log(i);
+    i++;
+  }
+}
+
+function testeObjeto() {
+  const pessoa = {
+    nome: "Henrique",
+    idade: 21,
+    cpf: "123.456.789-00",
+  };
+
+  console.log(typeof pessoa);
+  console.log(pessoa);
+
+  console.log(pessoa.nome);
+  console.log(pessoa.idade);
+  console.log(pessoa.cpf);
+}
+
+function testeObjeto2() {
+  const generico = [12, "Henrique", 45];
+  console.log(typeof generico);
+  console.log(generico);
+
+  for (let i = 0; i < generico.length; i++) {
+    console.log(generico[i]);
+  }
+}
+
+function testeArrayComObjeto() {
+  const pessoas = [
+    { "nome": "Rosinei", "idade": 21 },
+    { "nome": "Igor", "idade": 21 },
+    { "nome": "Lucas", "idade": 21 }
+  ];
+  for (let i = 0; i < pessoas.length; i++) {
+    console.log(pessoas[i].nome, ': ', pessoas[i].idade);
+  }
+}
+
+function testeArrayComObjeto2(){
+    const pessoas = {
+        "nome": "Ilda",
+        "idade": 45,
+        "dependentes": [
+            'Ana', 'Paulo', 'Isaias'
+        ]
+    }
 }
