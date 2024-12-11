8187b993da6458384e826ac34dd019fc2015291c
Henrique <103958824+HenriqueSales1@users.noreply.github.com>
2024-12-11 20:02:46 -0300

    Atividade-12-10

--- Atividade-12-10/js/dist/index.dev.js
+++ Atividade-12-10/js/dist/index.dev.js
@@ -1,9 +1,10 @@
 "use strict";
 
 var ver = [false, false, false]; // const troca = (event) => {
-//   const iSplit = event.target.id.split('imagem');
-//   if (!(ver[Number(iSplit[1])])) {
-//     const img = document.getElementById('troca' + (iSplit[1]));
+//   const iSplit = event.target.id.split("troca");
+//   if (!ver[Number(iSplit[1])]) {
+//     const img = document.getElementById(`imagem${iSplit[1]}`);
+//     console.log(`imagem${iSplit[1]}`);
 //     img.src = event.target.src;
 //     ver[Number(iSplit[1])] = true;
 //   }
@@ -31,16 +32,16 @@ var limpa = function limpa(event) {
   var eSplit = event.target.id.split("troca");
 
   if (ver[Number(eSplit[1])]) {
-    var img = document.getElementById('troca' + eSplit[1]);
-    img.src = './img/joker.png';
+    var img = document.getElementById("troca" + eSplit[1]);
+    img.src = "./img/joker.png";
     ver[Number(eSplit[1])] = false;
   }
 };
 
 for (var i = 0; i <= 2; i++) {
-  document.getElementById("troca".concat(i)).addEventListener('click', limpa);
+  document.getElementById("troca".concat(i)).addEventListener("click", limpa);
 }
 
 for (var _i = 0; _i <= 9; _i++) {
-  document.getElementById("imagem".concat(_i)).addEventListener('click', troca);
+  document.getElementById("imagem".concat(_i)).addEventListener("click", troca);
 }
--- Atividade-12-10/js/index.js
+++ Atividade-12-10/js/index.js
@@ -1,9 +1,10 @@
 let ver = [false, false, false];
 
 // const troca = (event) => {
-//   const iSplit = event.target.id.split('imagem');
-//   if (!(ver[Number(iSplit[1])])) {
-//     const img = document.getElementById('troca' + (iSplit[1]));
+//   const iSplit = event.target.id.split("troca");
+//   if (!ver[Number(iSplit[1])]) {
+//     const img = document.getElementById(`imagem${iSplit[1]}`);
+//     console.log(`imagem${iSplit[1]}`);
 //     img.src = event.target.src;
 //     ver[Number(iSplit[1])] = true;
 //   }
@@ -28,16 +29,16 @@ const troca = (event) => {
 const limpa = (event) => {
   const eSplit = event.target.id.split("troca");
   if (ver[Number(eSplit[1])]) {
-    const img = document.getElementById('troca' + (eSplit[1]));
-    img.src = './img/joker.png';
+    const img = document.getElementById("troca" + eSplit[1]);
+    img.src = "./img/joker.png";
     ver[Number(eSplit[1])] = false;
   }
-}
+};
 
 for (let i = 0; i <= 2; i++) {
-  document.getElementById(`troca${i}`).addEventListener('click', limpa);
+  document.getElementById(`troca${i}`).addEventListener("click", limpa);
 }
 
 for (let i = 0; i <= 9; i++) {
-  document.getElementById(`imagem${i}`).addEventListener('click', troca);
+  document.getElementById(`imagem${i}`).addEventListener("click", troca);
 }
