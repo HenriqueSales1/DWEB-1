7fb252491705036bd7a59eccb619d3568d995843
Henrique <103958824+HenriqueSales1@users.noreply.github.com>
2024-12-23 09:17:30 -0300

    .

---
+++ JS-Course-Udemy/Aula27/dist/index.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var num1 = 9.54578;
+var num2 = Math.floor(num1);
+var num3 = Math.ceil(num1);
+console.log(num1);
+console.log(num2);
+console.log(num3);
---
+++ JS-Course-Udemy/Aula27/index.js
@@ -0,0 +1,7 @@
+let num1 = 9.54578;
+let num2 = Math.floor(num1);
+let num3 = Math.ceil(num1);
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
