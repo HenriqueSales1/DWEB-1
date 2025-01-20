007d1cb436a5d5ce46e8af943a39212ba9bdaf0e
Henrique <103958824+HenriqueSales1@users.noreply.github.com>
2025-01-20 07:15:53 -0300

    intro php/bd

--- Aula-01-14/index.php
+++ Aula-01-14/index.php
@@ -1,7 +1,32 @@
-<?php
-require_once './controleCarta.php';
+<!DOCTYPE html>
+<html lang="en">
 
-$controle = new controleCarta();
-$controle->inserir();
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-var_dump($controle->inserir());
+<body>
+    <?php
+    require_once './controleCarta.php';
+
+    $controle = new controleCarta();
+    $lista = $controle->listar();
+
+    var_dump($controle->inserir());
+
+    foreach($lista as $carta){
+        echo '<h2>';
+        echo $carta['nome'];
+        echo '</h2>';
+        echo '<p>';
+        echo $carta['descricao'];
+        echo '</p>';
+    }
+
+    ?>
+
+</body>
+
+</html>
